ecd3033ae75c11d2b8d9aa543579b6f4475219e4
unknown <andreybyk9606@gmail.com>
2018-06-14 09:26:52 +0300

    browsersync was added

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,34 @@ var gulp =require('gulp')
     less=require('gulp-less')
     cleanCSS = require('gulp-clean-css')
     concatCss = require('gulp-concat-css');
-    
+var browserSync = require('browser-sync').create();
 var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
     autoprefixPlugin = new LessPluginAutoPrefix({browsers: ["last 20 versions"]});
 
-gulp.task('default',function(){
-    return gulp.watch('./app/less/*.less',function(){
+
+    /**
+     * less
+     */
+gulp.task('less',function(){
+    
         console.log('rewrite', (new Date()).getMinutes());
         return gulp.src('./app/less/*.less')
             .pipe(less({ plugins: [autoprefixPlugin] }))
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(concatCss("style.css"))
-            .pipe(gulp.dest('./app/css'));
-    });
+            .pipe(gulp.dest('./app/css'))
+            .pipe(browserSync.stream());
+    
 });
+/**browsersync
+ * 
+ */
+gulp.task('serve',['less'],function(){
+    browserSync.init({
+        server:"./app"
+    });
+    gulp.watch("app/less/*.less",['less']);
+    gulp.watch("app/*.html").on("change",browserSync.reload);
+
+});
+gulp.task('default',['serve']);
